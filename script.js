1711e4cff18f6fc229df9cb5d73f6399110734af
ImBatman-7 <sahuababhinav@gmail.com>
2021-05-19 19:49:12 +0530

    added comments and added a seperate SDK file

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+// script here
 
-// VoiceRSS Javascript SDK
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
 
-const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
-
+// toggling buttons
 function disableEnableButton(){
     button.disabled = !button.disabled;
 }
 
+// speech code
 function test(){
     VoiceRSS.speech({
         key: '3958b8e6243f4a538ed943f46e1cbbdc',
@@ -22,6 +22,7 @@ function test(){
     });
 }
 
+// getting jokes via jokeapi and managing jokes according to setup and delivery
 async function getJokes(){
     try{
         realJoke = '';
@@ -41,8 +42,10 @@ async function getJokes(){
     }  
 }
 
+
+// event listerners
 button.addEventListener('click', getJokes);
 audioElement.addEventListener('ended', disableEnableButton)
 
-
+// run function to get jokes
 getJokes();
